7d5d5d4df8d6873ffd29f7a847de9d8b860d4676
MayuWaka <180781056@ccmailg.meijo-u.ac.jp>
2020-05-17 14:23:49 +0900

    カウントアプリday4完成

---
+++ app/src/main/java/app/wakayama/tama/countday4/MainActivity.kt
@@ -0,0 +1,20 @@
+package app.wakayama.tama.countday4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number:Int = 0
+
+        button.setOnClickListener {
+            number = number + 1
+            text.text = number.toString()
+        }
+    }
+}
